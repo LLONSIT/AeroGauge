bc29d5151f309d2835257c9204a2a49175f1358d
LLONSIT <lonsitsyt456@gmail.com>
2022-11-12 18:14:59 -0500

    updating

---
+++ src/os/gu/cosf.c
@@ -0,0 +1,3 @@
+
+
+#pragma GLOBAL_ASM("asm/nonmatchings/os/cosf/__cosf.s")
---
+++ src/os/gu/coss.c
@@ -0,0 +1,8 @@
+#include <ultra64.h>
+
+signed short coss( unsigned short x )
+{
+	/* 0 <= x < 0x10000  ==>  0 <= x < 2PI */
+
+	return sins ((unsigned short) (x + 0x4000));
+}
