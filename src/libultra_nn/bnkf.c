bd3b53cd43b3ee744f78b1cdf381de4f48d5734e
LLONSIT <lonsitsyt456@gmail.com>
2022-12-05 18:05:51 -0500

    new libultra segs

--- src/libultra_nn/bnkf.c
+++ src/libultra_nn/bnkf.c
@@ -1,14 +1,14 @@
-#include "common.h"
+#include "macros.h"
 
-void func_80065BA0(void) {
+UNUSED func_80065BA0(void) {
 }
 
-void func_80065BA8(void) {
+UNUSED func_80065BA8(void) {
 }
 
 #pragma GLOBAL_ASM("asm/nonmatchings/libultra_nn/bnkf/func_80065BB0.s")
 
-void func_80065C9C(void) {
+UNUSED func_80065C9C(void) {
 }
 
 #pragma GLOBAL_ASM("asm/nonmatchings/libultra_nn/bnkf/func_80065CA4.s")
--- src/libultra_nn/sp.c
+++
@@ -1,3 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra_nn/sp/func_800787D0.s")
