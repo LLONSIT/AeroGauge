d403b21b6e332c9d8995e7125dad7edd3d885939
LLONSIT <lonsitsyt456@gmail.com>
2024-11-23 23:49:01 -0500

    Decompile some functions

--- src/66230.c
+++ src/66230.c
@@ -1,7 +1,34 @@
-#include "common.h"
+#include <ultra64.h>
 
-#pragma GLOBAL_ASM("asm/nonmatchings/66230/func_80065630.s")
+void func_80065E60(void *, u32);                       /* extern */
+extern u8 D_80092E10;
+extern void *D_801A4240;
+extern OSMesgQueue D_801A4560;
+extern void *D_801A4578;
+extern OSMesgQueue D_801A4580;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/66230/func_80065690.s")
+void func_80065630(void) {
+    if (D_80092E10 != 0) {
+        osCreatePiManager(0x96, &D_801A4560, &D_801A4240, 0xC8);
+        osCreateMesgQueue(&D_801A4580, &D_801A4578, 1);
+        D_80092E10 = 0;
+    }
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/66230/func_80065704.s")
+void func_80065690(u32 arg0, void *arg1, u32 arg2) {
+    OSIoMesg sp30;
+    void *sp2C;
+
+    func_80065E60(arg1, arg2);
+    osPiStartDma(&sp30, 0, 0, arg0, arg1, arg2, &D_801A4580);
+    osRecvMesg(&D_801A4580, &sp2C, 1);
+}
+
+/* TODO is this right ? */
+void func_80065704(u32 *arg0, u32 *arg1) {
+    u32 temp_a0;
+
+    temp_a0 = arg1[0];
+    func_80065690(temp_a0, arg0, arg1[1] - temp_a0);
+}
+
